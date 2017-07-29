a6d979c315954cc72dda5b71ea67c51c74bce797
ijmccallum <ijmccallum@hotmail.co.uk>
2017-07-29 10:20:01 -0400

    active links

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
+
 const StyledLink = styled(Link)`
   padding: 10px;
   font-style: italic;
@@ -10,20 +11,43 @@ const StyledLink = styled(Link)`
   text-decoration: none;
 `;
 
-//note the activeClassName needs this component to rerender when routes change - currently I don't believe it is
+const StyledActiveLink = styled(Link)`
+  padding: 10px;
+  font-style: italic;
+  color: white;
+  display: inline-block;
+  text-decoration: none;
+  font-weight: bold;
+`;
+
+function NavLink(props){
+  console.log("this.props'", props);
+  if (props.routeProps.location.pathname === props.url) {
+    return (
+      <StyledActiveLink to={props.url}>
+        {props.text}
+      </StyledActiveLink>
+    );
+  } else {
+    return (
+      <StyledLink to={props.url}>
+        {props.text}
+      </StyledLink>
+    );
+  }
+}
 
 function Nav(props){
-  console.log('props!', props);
   return (
     <nav className="nav">
-      <StyledLink to="/" className="nav__link" activeClassName="nav__link--active">Dundas Castle</StyledLink>
-      <StyledLink to="/accommodation" className="nav__link" activeClassName="nav__link--active">Accomodation</StyledLink>
-      <StyledLink to="/edinburgh" className="nav__link" activeClassName="nav__link--active">Edinburgh</StyledLink>
-      <StyledLink to="/gifts" className="nav__link" activeClassName="nav__link--active">Gifts</StyledLink>
-      <StyledLink to="/music" className="nav__link" activeClassName="nav__link--active">Music</StyledLink>
-      <StyledLink to="/rsvp" className="nav__link" activeClassName="nav__link--active">RSVP</StyledLink>
-      <StyledLink to="/transport" className="nav__link" activeClassName="nav__link--active">Transport</StyledLink>
-      <StyledLink to="/wedding-party" className="nav__link" activeClassName="nav__link--active">WeddingParty</StyledLink>
+      <NavLink url="/" text="Dundas Castle" routeProps={props} />
+      <NavLink url="/accommodation" text="Accomodation" routeProps={props} />
+      <NavLink url="/edinburgh" text="Edinburgh" routeProps={props} />
+      <NavLink url="/gifts" text="Gifts" routeProps={props} />
+      <NavLink url="/music" text="Music" routeProps={props} />
+      <NavLink url="/rsvp" text="RSVP" routeProps={props} />
+      <NavLink url="/transport" text="Transport" routeProps={props} />
+      <NavLink url="/wedding-party" text="Wedding Party" routeProps={props} />
     </nav>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, Link } from 'react-router-dom';
 import createHashHistory from 'history/createHashHistory';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import styled from 'styled-components';
+
+import Nav from './Components/Nav.jsx';
 
 //Scenes
 import DundasCastle from './Scenes/DundasCastle.jsx';
@@ -14,57 +16,48 @@ import Music from './Scenes/Music.jsx';
 import RSVP from './Scenes/RSVP.jsx';
 import Transport from './Scenes/Transport.jsx';
 import WeddingParty from './Scenes/WeddingParty.jsx';
-import styled from 'styled-components';
+
+const StyledContainer = styled.main`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`;
+
+const StyledHeader = styled.header`
+  background-color: #323232;
+  padding; 4px;
+  color: white;
+`;
+
+const StyledFooter = styled.footer`
+  margin-top: auto;
+`;
 
 const history = createHashHistory();
 
 ReactDOM.render((
   <Router history={history}>
-    <Route path="/">
-      <App>
-        <Route
-          path="/"
-          exact
-          title="Dundas Castle"
-          component={DundasCastle}
-        />
-        <Route
-          path="/accommodation"
-          title="Accommodation"
-          component={Accommodation}
-        />
-        <Route
-          path="/edinburgh"
-          title="Edinburgh"
-          component={Edinburgh}
-        />
-        <Route
-          path="/gifts"
-          title="Gifts"
-          component={Gifts}
-        />
-        <Route
-          path="/music"
-          title="Music"
-          component={Music}
-        />
-        <Route
-          path="/rsvp"
-          title="RSVP"
-          component={RSVP}
-        />
-        <Route
-          path="/transport"
-          title="Transport"
-          component={Transport}
-        />
-        <Route
-          path="/wedding-party"
-          title="Wedding Party"
-          component={WeddingParty}
-        />
-      </App>
-    </Route>
+    <StyledContainer>
+      <StyledHeader>
+        <Route path="/" component={Nav} />
+      </StyledHeader>
+
+      <div className="app__body">
+        <Route path="/" exact component={DundasCastle} />
+        <Route path="/accommodation" exact component={Accommodation} />
+        <Route path="/edinburgh" exact component={Edinburgh} />
+        <Route path="/gifts" exact component={Gifts} />
+        <Route path="/music" exact component={Music} />
+        <Route path="/rsvp" exact component={RSVP} />
+        <Route path="/transport" exact component={Transport} />
+        <Route path="/wedding-party" exact component={WeddingParty} />
+      </div>
+
+      <StyledFooter>
+        footer
+      </StyledFooter>
+    </StyledContainer>
   </Router>
 ), document.getElementById('root'));
+
 registerServiceWorker();

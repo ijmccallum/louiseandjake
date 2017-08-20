71ad9bfb613a2e03f8eea1904264991e6a254684
ijmccallum <ijmccallum@hotmail.co.uk>
2017-08-20 15:54:06 -0400

    home page content

--- src/Scenes/Home.jsx
+++ src/Scenes/Home.jsx
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
-import Page from '../Components/Page.jsx';
+import HeroPic from '../Components/HeroPic.jsx';
+import PageTitle from '../Components/PageTitle.jsx';
+import SubTitle from '../Components/SubTitle.jsx';
+import Section from '../Components/Section.jsx';
 
 class Home extends Component {
   render() {
     return (
-      <Page 
-        title="Louise & Jake" 
-        heroPic="louise-jake.jpg" 
-        backgroundSize="contain"
-        isLive={this.props.isLive}
-      >
-        September 1st 2018
-      </Page>
+      <div>
+        <PageTitle>
+          Louise & Jake’s<br />Wedding
+        </PageTitle>
+
+        <SubTitle>
+          Save the Date | 01.09.18
+        </SubTitle>
+
+        <HeroPic fileName={"louise-jake.jpg"}/>
+
+        <Section>
+          <p>
+            Please Save the Date Saturday 1st September 2018 for the Marriage of<br />
+            Louise Kirsteen McCallum & Jake Edward Michael Shackleford<br />
+            at Dundas Castle, Edinburgh
+          </p>
+
+          <p>RSVP: dundaswedding2018@gmail.com</p>
+
+          <p>Please let us know if you can attend our wedding, and also if you know you cannot. Let us know any dietary requirements and/or allergies.</p>
+        </Section>
+
+      </div>
     );
   }
 }
